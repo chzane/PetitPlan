dfe45aa89aa715cb31b92244913f37ab144fc678
chzane <986686113@qq.com>
2025-07-20 18:09:06 +0800

    chore:重构Icon组件逻辑，使其能够通过className更改颜色

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,9 +1,9 @@
-import MapPin from '../assets/icon/map-pin.svg';
-import CalendarTime from '../assets/icon/calendar-time.svg';
-import Link from '../assets/icon/link.svg';
-import Box from '../assets/icon/box.svg';
+import { ReactComponent as MapPin } from '../assets/icon/map-pin.svg';
+import { ReactComponent as CalendarTime } from '../assets/icon/map-pin.svg';
+import { ReactComponent as Link } from '../assets/icon/map-pin.svg';
+import { ReactComponent as Box } from '../assets/icon/map-pin.svg';
 
-const IconList: Record<string, string> = {
+const IconList: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
     'map-pin': MapPin,
     'calendar-time': CalendarTime,
     'link': Link,
@@ -14,21 +14,20 @@ interface IconProps {
     name: string;   // 图标名称
     w?: string;   // 图标宽度
     h?: string;   // 图标高度
-    alt?: string;   // 图标描述
     className?: string;   // class
     [key: string]: any;   // 其他属性
 }
 
 // 图标组件
-function Icon({ name, w = '18px', h = '18px', alt = '', className = '', ...props }: IconProps) {
-    const src = IconList[name];
+function Icon({ name, w = '18px', h = '18px', className = '', ...props }: IconProps) {
+    const SvgIcon = IconList[name];
 
-    if (!src) {
+    if (!SvgIcon) {
         console.warn(`Icon "${name}" not found in IconList.`);
         return null;
     }
 
-    return <img src={src} alt={alt || name} className={`${className}`} style={{ width: w, height: h }} {...props} />;
+    return <SvgIcon className={className} style={{ width: w, height: h }} {...props} />;
 }
 
 export default Icon;
