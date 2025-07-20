dd27181bccbd9a94d42f1abbe0e9250ddd6c635d
chzane <986686113@qq.com>
2025-07-20 15:23:29 +0800

    chore:添加了Tailwind CSS

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ function HomePage()
 {
     return (
         <>
-            <h1>Hello world!</h1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     );
 }
